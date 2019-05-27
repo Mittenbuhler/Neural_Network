58d71af63fbaa0a75506dfd8e4f8b77fba3095b2
Maximilian Mittenbühler <max.mittenbuhler@student.uva.nl>
2019-05-27 18:16:52 +0200

    Put into one script and fix pip

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
-from setuptools import setup
- 
-setup(
-    name='Digit_Recognition_NN',    
-    version='0.1',                         
-    scripts=['nn_functions.py', 'install_network.py', 'digit_recognition.py']
-    )
+import setuptools
+
+setuptools.setup(
+    name="digit_recognition",
+    version="0.0.6",
+    py_modules=['nn_functions', 'install_network', 'digit_recognition'],
+    package_dir={'': 'src'},
+    author="Maximilian Mittenbuhler",
+    author_email="max.mittenbuhler@student.uva.nl",
+    description="Neural network for digit recognition",
+    url="https://github.com/Mittenbuhler/Neural_Network.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
